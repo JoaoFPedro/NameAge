5ac4744c372643e132ba0affffad50b6b707dfd3
João Pedro <joao.pedro@dtidigital.com.br>
2023-09-16 20:28:14 -0300

    first commit - starting to structure the projet

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p> Username</p>
+        <input></input>
+        <p> Age (Years)</p>
+        <input></input>
+        <div className="button">
+          <button> Add User</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
