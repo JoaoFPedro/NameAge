f1d349e334bf0f03f5961bab03964123327b95f5
João Pedro <joao.pedro@dtidigital.com.br>
2023-09-20 19:38:52 -0300

    adding User Handler

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from "react";
+const DummyNames = [
+  {
+    name: "Jones Pones",
+    age: 25,
+  },
+  {
+    name: "Thiago Fragoso",
+    age: 12,
+  },
+];
 
 function App() {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <div>
-        <p> Username</p>
-        <input></input>
-        <p> Age (Years)</p>
-        <input></input>
-        <div className="button">
-          <button> Add User</button>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username"></input>
+        <label htmlFor="age">Age (years)</label>
+        <input type="text" id="age"></input>
+
+        <button type="submit"> Add User</button>
+      </form>
+      {DummyNames.map((user, index) => (
+        <div key={index}>
+          <p>
+            {user.name}, ({user.age} years old)
+          </p>
         </div>
-      </div>
+      ))}
+      ;
     </>
   );
 }
